8950ed886c537f04437cbdda17bf8dcf86d5fac4
acho <opehn0@gmail.com>
2024-10-01 17:12:56 +0900

    refactor: Command 클래스 리팩토링
    - 제약사항 검증하는 부분 함수로 분리
    - Command를 상수 하나하나로 관리하지 않고 CommandType으로 합쳐서 관리

--- src/const/command.ts
+++
@@ -1,2 +0,0 @@
-export const START_GAME: number = 1;
-export const END_GAME: number = 9;
--- src/domain/Command.ts
+++ src/domain/Command.ts
@@ -1,12 +1,30 @@
+export const CommandType = {
+    START_GAME: 1,
+    END_GAME: 9,
+} as const;
+
+type CommandTypeValues = (typeof CommandType)[keyof typeof CommandType];
+
 export class Command {
-    private readonly command: number;
+    private readonly command: CommandTypeValues;
 
     constructor(input: string) {
         const inputNumber = parseInt(input);
+        this.validateType(inputNumber);
+        this.validateCommand(inputNumber);
+        this.command = inputNumber as CommandTypeValues;
+    }
+
+    validateType(inputNumber: number) {
         if (isNaN(inputNumber)) {
             throw new Error('숫자를 입력해주세요');
         }
-        this.command = inputNumber;
+    }
+
+    validateCommand(inputNumber: number) {
+        if (!(Object.values(CommandType) as number[]).includes(inputNumber)) {
+            throw new Error('존재하지 않는 명령어입니다');
+        }
     }
 
     getCommand() {
